fba772d928498df1fcffa9c7709f31e3c6081d53
l-pires <pereirapireslucas@gmail.com>
2026-02-07 19:59:32 -0300

    feat: enable cors for microservices/linha-do-tempo

--- microservices/linha-do-tempo/src/app.js
+++ microservices/linha-do-tempo/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import api from './api/index.js';
 import { notFound, errorHandler } from './middlewares.js';
@@ -8,6 +9,7 @@ import { notFound, errorHandler } from './middlewares.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
